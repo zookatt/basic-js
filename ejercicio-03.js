70a238b9f1d1f256a7c7f0f934231562c514b7e9
zookat <zotova.ea@gmail.com>
2026-03-09 13:52:15 +0100

    Feat: ejercicio 3 — Strings y métodos

---
+++ ejercicio-03.js
@@ -0,0 +1,14 @@
+// ### Ejercicio 3: Posición de una letra
+
+// La frase `"My favorite dessert is jello"` contiene la letra `j`.
+
+// Escribe un programa que encuentre en qué posición (índice) está esa letra y lo muestre por consola.
+
+// **Pista:** los strings tienen un método que devuelve la posición de un carácter dentro de la cadena.
+// **Documentación:** [String.indexOf()](https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf)
+
+let frase = "My favorite dessert is jello";
+console.log(frase);
+let indexOfJ=frase.indexOf("j");
+console.log(indexOfJ)
+
